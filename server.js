414af65bb42eabaa8150dea4218f7ab75a390152
Paul Brickley <bricklep@gmail.com>
2024-07-19 18:44:32 -0700

    got working with interface selection and output

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const ping = require('net-ping');
+const path = require('path');
+const os = require('os');
 
 const app = express();
 const port = 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/interfaces', (req, res) => {
+  const interfaces = os.networkInterfaces();
+  const interfaceList = [];
+  for (const [name, infos] of Object.entries(interfaces)) {
+    for (const info of infos) {
+      if (info.family === 'IPv4' && !info.internal) {
+        interfaceList.push({ name, address: info.address });
+      }
+    }
+  }
+  res.json(interfaceList);
+});
 
 app.get('/scan', (req, res) => {
   const session = ping.createSession();
@@ -16,6 +34,7 @@ app.get('/scan', (req, res) => {
       session.pingHost(ip, (error, target) => {
         if (!error) {
           devices.push(target);
+          console.log(`${target} is alive`);
         }
         resolve();
       });
